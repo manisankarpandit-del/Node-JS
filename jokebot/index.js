349efce7ca86c37310611e6d267223f80773c013
Mani Sankar Pandit <msankarpandit@gmail.com>
2026-05-13 17:33:47 +0530

    add

---
+++ jokebot/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+
+const TelegramBot =  require('node-telegram-bot-api');
+
+const token = '8930786939:8930786939:AAEhIpY1pkPX9X2Dcn_a03lC0MB5oZKiTYQ';
+
+console.log(process.env.BOT_TOKEN);
